247ddfbb019c90ac3f987d9bb88e23e3e4ba4445
anmcqueen <asetorn@yandex.ru>
2023-06-28 22:54:27 +0300

    3# Установка Sock-shop переделана на Helm

--- resource-installation.sh
+++ resource-installation.sh
@@ -1,3 +1,4 @@
+# TODO прописать верные пути к файлам
 # Предварительно установлен helm "v3.11.2"
 
 # Создан alias
@@ -21,14 +22,14 @@ helm install \
   --create-namespace \
   --version v1.12.0 \
   --set installCRDs=true
-kl apply -f ClusterIssuer-letsencrypt.yaml
-kl apply -f Сertificates.yaml
+kl apply -f cert-manager/ClusterIssuer-letsencrypt.yaml
 
 
 # Установка Sock Shop (будет переделано)
-kl apply -f sock-shop/complete-demo.yaml
-# Ingress для sock-shop (frontend)
-kl apply -f sock-shop/frontend-ingress.yaml
+kl create ns sock-shop
+kl apply -f sock-shop/sock-shop-certificates.yaml
+helm upgrade --install --create-namespace sock-shop -n sock-shop kubernetes-final-work/helm-charts/sock-shop
+
 
 # Monitoring
 # Prometheus
@@ -40,6 +41,7 @@ helm repo add grafana https://grafana.github.io/helm-charts
 helm upgrade --install --create-namespace --values grafana-values.yaml grafana -n monitoring grafana/grafana
 # Получила логин-пароль из секрета grafana
 kl get secret -n monitoring grafana -o json
+kl apply -f monitoring/grafana-certificates.yaml
 # Ingress для sock-shop (frontend)
 kl apply -f Ingress/grafana-ingress-no-tls.yaml
 
